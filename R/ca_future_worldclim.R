1d7f91f86a863db22a5640024e8d1439f169892c
ManuelSpinola <mspinola10@gmail.com>
2023-12-27 15:03:07 -0600

    include gcm

--- R/ca_future_worldclim.R
+++ R/ca_future_worldclim.R
@@ -18,7 +18,12 @@
 #' download. Should be one of 0.5, 2.5, 5, 10. The default is 10.
 #'
 #' @param gcm (\code{character}) The option for global climate models.
-#' Check https://www.worldclim.org for all available GCM.
+#' Check https://www.worldclim.org for all available GCM. Those are, "ACCESS-CM2", "ACCESS-ESM1-5", "BCC-CSM2-MR", "CanESM5",
+"CanESM5-CanOE", "CMCC-ESM2", "CNRM-CM6-1", "CNRM-CM6-1-HR",
+"CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3-Veg-LR", "FIO-ESM-2-0",
+"GISS-E2-1-G", "GISS-E2-1-H", "HadGEM3-GC31-LL", "INM-CM4-8",
+"INM-CM5-0", "IPSL-CM6A-LR", "MIROC-ES2L", "MIROC6",
+"MPI-ESM1-2-HR", "MPI-ESM1-2-LR", "MRI-ESM2-0", "UKESM1-0-LL"
 #'
 #' @param ssp (\code{character}) The option for Shared Socio-economic Pathways.
 #' Should be one of "ssp126", "ssp245", "ssp370", "ssp585".
